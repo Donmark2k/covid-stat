c4e0c90c5a31ce39adf964db89d56405a114b7ec
Donmark2k <chukwuemekavp@gmail.com>
2023-03-13 11:01:08 +0100

    Update License

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar';
-import Missions from './components/mission';
-import Rockets from './components/rocket';
-import Profile from './components/profile';
+import Cities from './components/cities';
+import CityDetails from './components/cityDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,9 +9,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Cities />} />
+      <Route path="/:cityID" element={<CityDetails />} />
       </Routes>
     </div>
   );
