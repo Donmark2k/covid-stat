72664d08dcebf92f617071c47e7df8385f89378b
Donmark2k <chukwuemekavp@gmail.com>
2023-03-14 05:31:28 +0100

    Create store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,21 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import { createLogger } from 'redux-logger';
-// import rocketReducer from './rockets/rocketSlice';
-// import missionReducer from './missions/missionSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+import detailReducer from './detail/DetailSlice';
 
-// const logger = createLogger({
-//   // Options for the logger can be passed here
-//   // Show only the actions in the console
-//   predicate: (getState, action) => action.type !== 'SOME_ACTION',
-//   // Collapse the logs by default
-//   collapsed: true,
-// });
+const logger = createLogger({
+  // Options for the logger can be passed here
+  // Show only the actions in the console
+  predicate: (getState, action) => action.type !== 'SOME_ACTION',
+  // Collapse the logs by default
+  collapsed: true,
+});
 
-// export const store = configureStore({
-//   reducer: {
-//     missions: missionReducer,
-//     rockets: rocketReducer,
-//   },
-//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-// });
+const store = configureStore({
+  reducer: {
+    country: homeReducer,
+    city: detailReducer,
+  },
+  applyMiddleware:[thunk, logger],
+});
 
-// export default store;
+export default store;
