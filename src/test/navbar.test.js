312f3f573ad10475dcbe18c0f63b515e2a8b2295
Donmark2k <chukwuemekavp@gmail.com>
2023-03-17 12:20:50 +0100

    navbar.test.js is passing

--- src/test/navbar.test.js
+++ src/test/navbar.test.js
@@ -2,34 +2,49 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
-import {store} from '../redux/store';
-// import NavBar from '../components/navbar';
+import { store } from '../redux/store';
+import NavBar from '../components/navbar';
 import App from '../App';
+import { ErrorBoundary } from 'react-error-boundary';
 
-// it('Navbar page renders correctly', () => {
-//   const tree = renderer
-//     .create(
-//       <React.StrictMode>
-//         <Provider store={store}>
-//           <BrowserRouter>
-//             <NavBar />
-//           </BrowserRouter>
-//         </Provider>
-//       </React.StrictMode>,
-//     )
-//     .toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+function MyFallbackComponent({ error, resetErrorBoundary }) {
+  return (
+    <div>
+      <h2>Something went wrong:</h2>
+      <p>{error.message}</p>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
+it('Navbar page renders correctly', () => {
+  const tree = renderer
+    .create(
+      <React.StrictMode>
+        <ErrorBoundary FallbackComponent={MyFallbackComponent}>
+          <Provider store={store}>
+            <BrowserRouter>
+              <NavBar />
+            </BrowserRouter>
+          </Provider>
+        </ErrorBoundary>
+      </React.StrictMode>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 it('App  page renders correctly', () => {
   const tree = renderer
     .create(
       <React.StrictMode>
-        <Provider store={store}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Provider>
+        <ErrorBoundary FallbackComponent={MyFallbackComponent}>
+          <Provider store={store}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </Provider>
+        </ErrorBoundary>
       </React.StrictMode>,
     )
     .toJSON();
